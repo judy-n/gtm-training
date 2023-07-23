8f305eb4519363ed9e1427afd9f4bfcc1346b33d
judy-n <judynaamani@icloud.com>
2023-07-22 21:07:39 -0400

    updates to home page

--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -8,7 +8,8 @@ import React from "react";
 
 export const AppBar = () => {
   return (
-    <MuiAppBar position={"static"}>
+    <MuiAppBar position={"static"} sx={{zIndex: 10}}>
+               {/*// sx={{background: '#193a61'}}*/}
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           GTM Playground
--- src/components/home/homepage.tsx
+++ src/components/home/homepage.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, Paper, Stack, Toolbar, Typography } from "@mui/material";
+import { Divider, Paper, Stack, Typography } from "@mui/material";
 import React from "react";
 import GtmLogo from "../../assets/gtm-logo.png";
 import { AppBar } from "../appbar";
@@ -9,8 +9,15 @@ export const Homepage: React.FC = () => {
   return (
     <Stack>
       <AppBar />
-      <Stack p={1} alignItems={"center"}>
-        <Paper sx={{ width: "fit-content", p: 2 }}>
+      <Stack p={1} alignItems={"center"} gap={4}
+             direction={"row"}
+             justifyContent={"center"}
+             sx={{
+        zIndex: 0,
+        background: '#18324b',
+        borderBottom: 'solid 12px #1a76d2',
+        height: 300}}>
+        <Paper sx={{ width: "fit-content", p: 2, mt: 2 }}>
           <Stack direction={"row"} justifyContent={"center"}>
             <Stack direction={"row"} alignItems={"center"} columnGap={1}>
               <img style={{ width: 50 }} src={GtmLogo} alt={"GTM Logo"} />
@@ -23,17 +30,22 @@ export const Homepage: React.FC = () => {
             </Stack>
           </Stack>
         </Paper>
-        <Paper variant={"outlined"}>
+        <Divider orientation="vertical" flexItem color={'#89b3f8'} sx={{width:'2px', height: '30%', alignSelf: 'center', mt: 1}} />
+        <Stack sx={{color: 'white'}} direction={"row"} gap={2}>
           <Stack>
-            <Typography variant="h4" sx={{ flexGrow: 1 }}>
+            <Typography variant="h3" sx={{ flexGrow: 1 }}>
               GTM Playground
             </Typography>
             <Typography variant="h6" sx={{ flexGrow: 1 }}>
-              Demo site to practice using Google Tag Manager
+              Demo site to practice Google Tag Manager
             </Typography>
           </Stack>
-        </Paper>
+        </Stack>
       </Stack>
-    </Stack>
-  );
-};
+      <Stack mt={3}>
+        <Stack alignItems={'center'}>
+          <Typography variant={"h4"} color={'#18324b'}>Resources</Typography></Stack>
+      </Stack>
+      </Stack>
+  )
+}
